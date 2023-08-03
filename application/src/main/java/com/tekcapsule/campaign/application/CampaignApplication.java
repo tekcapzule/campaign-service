42c983078bd5f3cfda4bc6383c1c5f75a2534497
haritha <haritha.peryala@gmail.com>
2023-08-03 12:45:27 +0530

    API issue

--- application/src/main/java/com/tekcapsule/campaign/application/CampaignApplication.java
+++ application/src/main/java/com/tekcapsule/campaign/application/CampaignApplication.java
@@ -3,7 +3,7 @@ package com.tekcapsule.campaign.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.tekcapsule.market","com.tekcapsule.core"})
+@SpringBootApplication(scanBasePackages = {"com.tekcapsule.campaign","com.tekcapsule.core"})
 public class CampaignApplication {
     public static void main(String[] args) {
         SpringApplication.run(CampaignApplication.class, args);
