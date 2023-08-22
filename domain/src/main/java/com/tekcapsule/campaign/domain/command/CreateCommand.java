02349ecd825e08e040e0eed8875230dd0d7fba9f
Linjith <linjith.kunnon@gmail.com>
2023-08-22 08:02:38 +0530

    refactoring

--- domain/src/main/java/com/tekcapsule/campaign/domain/command/CreateCommand.java
+++ domain/src/main/java/com/tekcapsule/campaign/domain/command/CreateCommand.java
@@ -1,9 +1,12 @@
 package com.tekcapsule.campaign.domain.command;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.tekcapsule.campaign.domain.model.Highlight;
 import com.tekcapsule.core.domain.Command;
 import lombok.Builder;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -12,6 +15,7 @@ public class CreateCommand extends Command {
     private String title;
     private String subTitle;
     private String description;
+    private List<Highlight> highlights;
     private String startsOn;
     private String endsOn;
     private String offer;
