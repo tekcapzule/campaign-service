bf510c710caddf6b5c04d1bc05ece6733d6014a3
haritha <haritha.peryala@gmail.com>
2023-08-24 15:29:26 +0530

    Modified FindAll API

--- domain/src/main/java/com/tekcapsule/campaign/domain/repository/LeadDynamoRepository.java
+++ domain/src/main/java/com/tekcapsule/campaign/domain/repository/LeadDynamoRepository.java
@@ -1,12 +1,15 @@
 package com.tekcapsule.campaign.domain.repository;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.tekcapsule.campaign.domain.model.Lead;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
 import java.util.List;
 @Slf4j
 @Repository
@@ -26,8 +29,19 @@ public class LeadDynamoRepository implements LeadRepository{
 
     @Override
     public List<Lead> findAll() {
+        HashMap<String, AttributeValue> expAttributes = new HashMap<>();
+        expAttributes.put(":status", new AttributeValue().withS(ACTIVE_STATUS));
 
-        return dynamo.scan(Lead.class,new DynamoDBScanExpression());
+        HashMap<String, String> expNames = new HashMap<>();
+        expNames.put("#status", "status");
+
+        DynamoDBQueryExpression<Lead> queryExpression = new DynamoDBQueryExpression<Lead>()
+                .withConsistentRead(false)
+                .withKeyConditionExpression("#status = :status")
+                .withExpressionAttributeValues(expAttributes)
+                .withExpressionAttributeNames(expNames);
+
+        return dynamo.query(Lead.class, queryExpression);
     }
 
     @Override
